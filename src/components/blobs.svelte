e4f38fbd2b0792c957be0f2c67e5fb83243ba1a0
Paschal Ezeugwu <ezeugwupaschal@gmail.com>
2023-06-15 00:13:54 +0100

    Fix syntax error

--- src/components/blobs.svelte
+++ src/components/blobs.svelte
@@ -1,6 +1,7 @@
 <div class="absolute w-full h-full left-0 top-0 text-brand">
   <div class="w-[15%] absolute bottom-[100px] right-[20px] inline-block opacity-20">
     <Blob2 />
+  </div>
 </div>
 
 <script>
