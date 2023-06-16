5d2df3100c33273a323e20ee8051e1261df29095
Paschal Ezeugwu <ezeugwupaschal@gmail.com>
2023-06-16 20:11:07 +0100

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,11 @@
 <svelte:head>
   <title>{APP_NAME} - {APP_TAGLINE}</title>
-  <meta name="description" content="{APP_DESCRIPTION}" />
+  <meta property="og:type" content="website">
   <meta property="og:url" content="{APP_URL}"/> 
   <meta property="og:title" content="{APP_NAME}"/>  
+  <meta name="description" content="{APP_DESCRIPTION}" />
   <meta property="og:description" content="{APP_DESCRIPTION}"/>
-  <meta property="og:image" content="/graph.jpg">  
+  <meta property="og:image" content="/graph.jpg">
 </svelte:head>
 
 <script>
